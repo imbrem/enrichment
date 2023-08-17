962ee5184f8b9cb5c8e0c86e624ba9e49cd13069
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2023-08-17 01:21:24 +0100

    Generalized pure semantics, semantic inclusion

--- Enrichment/Runtime/Diagram.lean
+++ Enrichment/Runtime/Diagram.lean
@@ -59,6 +59,18 @@ inductive Diagram {C: Type u}
 | pure {X Y: C}: (Value.box X ⟶ Value.box Y) -> Diagram ⟨X, 0⟩ ⟨Y, 0⟩
 | effectful {X Y: C}: (X ⟶ Y) -> Diagram ⟨tensorUnit' ⊗ X, 1⟩ ⟨tensorUnit' ⊗ Y, 1⟩
 
+def Diagram.is_pure {C: Type u}
+  [TensorMonoid C]
+  [Quiver.{v} (Value C)]
+  [Quiver.{v} C]
+  {X Y: DiagramPort C}: Diagram X Y -> Prop
+  | identity X => True
+  | comp f g => f.is_pure ∧ g.is_pure 
+  | whiskerLeft X f => f.is_pure
+  | whiskerRight f X => f.is_pure
+  | effectful _ => False
+  | _ => True
+
 theorem Diagram.state_conservation
   {C: Type u}
   [TensorMonoid C]
@@ -89,6 +101,20 @@ theorem Diagram.no_forgetting
   {X Y: DiagramPort C}
   (D: Diagram X Y): X.states = 0 -> Y.states = 0
   := by simp [D.state_conservation]
+
+theorem Diagram.zero_left_pure {C: Type u}
+  [TensorMonoid C]
+  [Quiver.{v} (Value C)]
+  [Quiver.{v} C]
+  {X Y: DiagramPort C} (D: Diagram X Y)
+  (HX: X.states = 0)
+  : D.is_pure
+  := by induction D with
+  | comp f g If Ig => exact ⟨(If HX), (Ig (f.no_forgetting HX))⟩
+  | whiskerLeft X _ If => exact If (DiagramPort.zero_tensor_right HX)
+  | whiskerRight _ X If => exact If (DiagramPort.zero_tensor_left HX)
+  | effectful _ => cases HX
+  | _ => constructor
 
 def Diagram.semantics {C: Type u}
   [TensorMonoid C]
@@ -125,11 +151,11 @@ def Diagram.pure_semantics {C: Type u}
   [𝒱: PremonoidalCategory (Value C)]
   [𝒮: SymmetricPremonoidalCategory (Value C)]
   {X Y: DiagramPort C}
-  : Diagram X Y -> X.states = 0 -> (Value.box X.value ⟶ Value.box Y.value)
+  : (D: Diagram X Y) -> D.is_pure -> (Value.box X.value ⟶ Value.box Y.value)
 | identity X, _ => 𝟙 (Value.box X.value)
-| comp f g, Hx => f.pure_semantics Hx ≫ g.pure_semantics (f.no_forgetting Hx)
-| whiskerLeft Z f, Hx => 𝒱.whiskerLeft Z.value (f.pure_semantics (DiagramPort.zero_tensor_right Hx))
-| whiskerRight f Z, Hx => 𝒱.whiskerRight (f.pure_semantics (DiagramPort.zero_tensor_left Hx)) Z.value
+| comp f g, Hx => f.pure_semantics Hx.1 ≫ g.pure_semantics Hx.2
+| whiskerLeft X f, Hx => 𝒱.whiskerLeft X.value (f.pure_semantics Hx)
+| whiskerRight f X, Hx => 𝒱.whiskerRight (f.pure_semantics Hx) X.value
 | associator X Y Z, _ => (𝒱.associator X.value Y.value Z.value).hom
 | associator_inv X Y Z, _ => (𝒱.associator X.value Y.value Z.value).inv
 | leftUnitor X, _ => (𝒱.leftUnitor X.value).hom
@@ -137,8 +163,10 @@ def Diagram.pure_semantics {C: Type u}
 | rightUnitor X, _ => (𝒱.rightUnitor X.value).hom
 | rightUnitor_inv X, _ => (𝒱.rightUnitor X.value).inv
 | braiding X Y, _ => (𝒮.braiding X.value Y.value).hom
+| split, Hx => (𝒱.leftUnitor tensorUnit').inv
+| join, Hx => (𝒱.leftUnitor tensorUnit').hom
 | pure f, _ => f
-| split, Hx | join, Hx | effectful _, Hx => by cases Hx
+| effectful _, Hx => Hx.elim
 
 theorem Diagram.semantic_inclusion {C: Type u}
   [TensorMonoid C]
@@ -152,28 +180,28 @@ theorem Diagram.semantic_inclusion {C: Type u}
   [ℰ: EffectfulCategory C]
   [𝒮: SymmetricEffectfulCategory C]
   {X Y: DiagramPort C}
-  : (D: Diagram X Y) -> (H: X.states = 0) -> ℰ.inclusion.map' (D.pure_semantics H) = D.semantics
+  : (D: Diagram X Y) -> (H: D.is_pure) -> ℰ.inclusion.map' (D.pure_semantics H) = D.semantics
   | identity _, _ => ℰ.inclusion.map_id
   | comp f g, Hx => by
     rw [
       pure_semantics,
       ℰ.inclusion.map_comp',
-      f.semantic_inclusion Hx,
-      g.semantic_inclusion (f.no_forgetting Hx)
+      f.semantic_inclusion Hx.1,
+      g.semantic_inclusion Hx.2
     ]
     rfl
   | whiskerLeft Z f, Hx => by
     rw [
       pure_semantics,
       ℰ.inclusion_whiskerLeft,
-      f.semantic_inclusion (DiagramPort.zero_tensor_right Hx)
+      f.semantic_inclusion Hx
     ]
     rfl
   | whiskerRight f Z, Hx => by
     rw [
       pure_semantics,
       ℰ.inclusion_whiskerRight,
-      f.semantic_inclusion (DiagramPort.zero_tensor_left Hx)
+      f.semantic_inclusion Hx
     ]
     rfl
   | associator X Y Z, _ => ℰ.inclusion_associator X.value Y.value Z.value
@@ -184,7 +212,9 @@ theorem Diagram.semantic_inclusion {C: Type u}
   | rightUnitor_inv X, _ => ℰ.inclusion_rightUnitor_inv X.value
   | braiding X Y, _ => 𝒮.inclusion_braiding X.value Y.value
   | pure _, _ => rfl
-  | split, Hx | join, Hx | effectful _, Hx => by cases Hx
+  | split, Hx => ℰ.inclusion_leftUnitor_inv _
+  | join, Hx => ℰ.inclusion_leftUnitor _
+  | effectful _, Hx => by cases Hx
 
 inductive Diagram.inverses {C: Type u}
   [TensorMonoid C]
@@ -195,7 +225,6 @@ inductive Diagram.inverses {C: Type u}
 | leftUnitor (X): inverses (leftUnitor X) (leftUnitor_inv X)
 | rightUnitor (X): inverses (rightUnitor X) (rightUnitor_inv X)
 | braiding (X Y): inverses (braiding X Y) (braiding Y X)
-| symm {X Y} {f: Diagram X Y} {g: Diagram Y X}: inverses f g -> inverses g f
 
 theorem Diagram.inverses.semantics {C: Type u}
   [TensorMonoid C]
@@ -207,16 +236,13 @@ theorem Diagram.inverses.semantics {C: Type u}
   [PremonoidalCategory C]
   [𝒮: SymmetricPremonoidalCategory C]
   [EffectfulCategory C]
-  [SymmetricEffectfulCategory C]
   {X Y: DiagramPort C}
   (f: Diagram X Y) (g: Diagram Y X)
   (I: f.inverses g) 
   : f.semantics ≫ g.semantics = 𝟙 X.value ∧ g.semantics ≫ f.semantics = 𝟙 Y.value
-  := by induction I with
-  | symm _ I => exact ⟨I.2, I.1⟩
-  | _ => constructor <;> simp [Diagram.semantics, 𝒮.symmetry] <;> rfl
+  := by cases I <;> constructor <;> simp [Diagram.semantics, 𝒮.symmetry] <;> rfl
 
-inductive Diagram.isotopy {C: Type u}
+inductive Diagram.association {C: Type u}
   [TensorMonoid C]
   [Category (Value C)]
   [Category C]
@@ -225,89 +251,102 @@ inductive Diagram.isotopy {C: Type u}
   [MonoidalCategory' (Value C)]
   [PremonoidalCategory C]
   [ℰ: EffectfulCategory C]
+  (P: {X Y: DiagramPort C} -> Diagram X Y -> Prop)
   : {X Y: DiagramPort C} -> Diagram X Y -> Diagram X Y -> Prop
-| identity_left {X Y} (f: Diagram X Y): isotopy (comp f (identity Y)) f
-| identity_right {X Y} (f: Diagram X Y): isotopy (comp (identity X) f) f
-| comp_assoc {X Y Z W} {f: Diagram X Y} {g: Diagram Y Z} {h: Diagram Z W}
-  : isotopy (comp f (comp g h)) (comp (comp f g) h)
-| inv_comp {X Y} {f: Diagram X Y} {g: Diagram Y X}
-  : inverses f g -> isotopy (comp f g) (identity X)
-| whiskerLeft_identity (X Y)
-  : isotopy (whiskerLeft X (identity Y)) (identity (X ⊗ Y))
-| whiskerRight_identity (X Y)
-  : isotopy (whiskerRight (identity X) Y) (identity (X ⊗ Y))
-| sliding {X₁ Y₁ X₂ Y₂} (f: Diagram X₁ Y₁) (g: Diagram X₂ Y₂)
-  : isotopy 
-    (comp (whiskerRight f X₂) (whiskerLeft Y₁ g)) 
-    (comp (whiskerLeft X₁ g) (whiskerRight f Y₂))
-| associator_left {X Y Z X'} (f: Diagram X X')
-  : isotopy
-    (comp (whiskerRight (whiskerRight f Y) Z) (associator X' Y Z))
-    (comp (associator X Y Z) (whiskerRight f (Y ⊗ Z)))
-| associator_mid {X Y Z Y'} (f: Diagram Y Y')
-  : isotopy
-    (comp (whiskerRight (whiskerLeft X f) Z) (associator X Y' Z))
-    (comp (associator X Y Z) (whiskerLeft X (whiskerRight f Z)))
-| associator_right {X Y Z Z'} (f: Diagram Z Z')
-  : isotopy
-    (comp (whiskerLeft (X ⊗ Y) f) (associator X Y Z'))
-    (comp (associator X Y Z) (whiskerLeft X (whiskerLeft Y f)))
-| leftUnitor {X Y} (f: Diagram X Y)
-  : isotopy
-    (comp (leftUnitor X) f)
-    (comp (whiskerLeft tensorUnit' f) (leftUnitor Y))
-| rightUnitor {X Y} (f: Diagram X Y)
-  : isotopy
-    (comp (rightUnitor X) f)
-    (comp (whiskerRight f tensorUnit') (rightUnitor Y))
-| braiding_left {X Y Z} (f: Diagram X Y)
-  : isotopy
-    (comp (whiskerLeft Z f) (braiding Z Y))
-    (comp (braiding Z X) (whiskerRight f Z))
-| braiding_right {X Y Z} (f: Diagram X Y)
-  : isotopy
-    (comp (whiskerRight f Z) (braiding Y Z))
-    (comp (braiding X Z) (whiskerLeft Z f))
-| triangle (X Y)
-  : isotopy
-    (comp (associator X tensorUnit' Y) (whiskerLeft X (leftUnitor Y)))
-    (whiskerRight (rightUnitor X) Y)
-| pentagon (X Y Z W)
-  : isotopy
-    (comp (associator (X ⊗ Y) Z W) (associator X Y (Z ⊗ W)))
-    (comp (whiskerRight (associator X Y Z) W) 
-      (comp (associator X (Y ⊗ Z) W) (whiskerLeft X (associator Y Z W))))
-| hexagon (X Y Z)
-  : isotopy
-    (comp (associator X Y Z) (comp (braiding X (Y ⊗ Z)) (associator Y Z X)))
-    (comp (whiskerRight (braiding X Y) Z) (comp (associator Y X Z) (whiskerLeft Y (braiding X Z))))
-| hoop: isotopy (comp split join) (identity state')
-| pure_identity (X): isotopy (pure (𝟙 X)) (identity _)
-| pure_composes {X Y Z: C} (f: Value.box X ⟶ Value.box Y) (g: Value.box Y ⟶ Value.box Z)
-  : isotopy (comp (pure f) (pure g)) (pure (f ≫ g))
-| pure_left {X Y Z: C} (f: Value.box X ⟶ Value.box Y)
-  : isotopy (whiskerLeft ⟨Z, 0⟩ (pure f)) (pure (𝒱.whiskerLeft Z f))
-| pure_right {X Y Z: C} (f: Value.box X ⟶ Value.box Y)
-  : isotopy (whiskerRight (pure f) ⟨Z, 0⟩) (pure (𝒱.whiskerRight f Z))
-| pure_associator (X Y Z: C)
-  : isotopy (@pure C _ _ _ _ _ (𝒱.associator X Y Z).hom) (associator ⟨X, 0⟩ ⟨Y, 0⟩ ⟨Z, 0⟩)
-| pure_leftUnitor (X: C)
-  : isotopy (@pure C _ _ _ _ _ (𝒱.leftUnitor X).hom) (leftUnitor ⟨X, 0⟩)
-| pure_rightUnitor (X: C)
-  : isotopy (@pure C _ _ _ _ _ (𝒱.rightUnitor X).hom) (rightUnitor ⟨X, 0⟩)
-| pure_braiding (X Y: C)
-  : isotopy (@pure C _ _ _ _ _ (𝒮.braiding X Y).hom) (braiding ⟨X, 0⟩ ⟨Y, 0⟩)
-| effectful_identity (X: C)
-  : isotopy (effectful (𝟙 X)) (identity ⟨tensorUnit' ⊗ X, 1⟩)
---TODO: effectful whiskering?
-| effectful_inclusion_left {X Y Z: C} (f: X ⟶ Y) (g: Value.box Y ⟶ Value.box Z)
-  : isotopy 
-    (effectful (f ≫ ℰ.inclusion.map' g))
-    (comp (effectful f) (whiskerLeft state' (pure g)))
-| effectful_inclusion_right {X Y Z: C} (f: Value.box X ⟶ Value.box Y) (g: Y ⟶ Z)
-  : isotopy 
-    (effectful (ℰ.inclusion.map' f ≫ g))
-    (comp (whiskerLeft state' (pure f)) (effectful g))
+  | identity_left {X Y} (f: Diagram X Y): association P (comp f (identity Y)) f
+  | identity_right {X Y} (f: Diagram X Y): association P (comp (identity X) f) f
+  | comp_assoc {X Y Z W} {f: Diagram X Y} {g: Diagram Y Z} {h: Diagram Z W}
+    : association P (comp f (comp g h)) (comp (comp f g) h)
+  | inv_comp {X Y} {f: Diagram X Y} {g: Diagram Y X}
+    : inverses f g -> association P (comp f g) (identity X)
+  | whiskerLeft_identity (X Y)
+    : association P (whiskerLeft X (identity Y)) (identity (X ⊗ Y))
+  | whiskerRight_identity (X Y)
+    : association P (whiskerRight (identity X) Y) (identity (X ⊗ Y))
+  | sliding {X₁ Y₁ X₂ Y₂} (f: Diagram X₁ Y₁) (g: Diagram X₂ Y₂)
+    : P f ∨ P g -> association P 
+      (comp (whiskerRight f X₂) (whiskerLeft Y₁ g)) 
+      (comp (whiskerLeft X₁ g) (whiskerRight f Y₂))
+  | associator_left {X Y Z X'} (f: Diagram X X')
+    : association P
+      (comp (whiskerRight (whiskerRight f Y) Z) (associator X' Y Z))
+      (comp (associator X Y Z) (whiskerRight f (Y ⊗ Z)))
+  | associator_mid {X Y Z Y'} (f: Diagram Y Y')
+    : association P
+      (comp (whiskerRight (whiskerLeft X f) Z) (associator X Y' Z))
+      (comp (associator X Y Z) (whiskerLeft X (whiskerRight f Z)))
+  | associator_right {X Y Z Z'} (f: Diagram Z Z')
+    : association P
+      (comp (whiskerLeft (X ⊗ Y) f) (associator X Y Z'))
+      (comp (associator X Y Z) (whiskerLeft X (whiskerLeft Y f)))
+  | leftUnitor {X Y} (f: Diagram X Y)
+    : association P
+      (comp (leftUnitor X) f)
+      (comp (whiskerLeft tensorUnit' f) (leftUnitor Y))
+  | rightUnitor {X Y} (f: Diagram X Y)
+    : association P
+      (comp (rightUnitor X) f)
+      (comp (whiskerRight f tensorUnit') (rightUnitor Y))
+  | braiding_left {X Y Z} (f: Diagram X Y)
+    : association P
+      (comp (whiskerLeft Z f) (braiding Z Y))
+      (comp (braiding Z X) (whiskerRight f Z))
+  | braiding_right {X Y Z} (f: Diagram X Y)
+    : association P
+      (comp (whiskerRight f Z) (braiding Y Z))
+      (comp (braiding X Z) (whiskerLeft Z f))
+  | triangle (X Y)
+    : association P
+      (comp (associator X tensorUnit' Y) (whiskerLeft X (leftUnitor Y)))
+      (whiskerRight (rightUnitor X) Y)
+  | pentagon (X Y Z W)
+    : association P
+      (comp (associator (X ⊗ Y) Z W) (associator X Y (Z ⊗ W)))
+      (comp (whiskerRight (associator X Y Z) W) 
+        (comp (associator X (Y ⊗ Z) W) (whiskerLeft X (associator Y Z W))))
+  | hexagon (X Y Z)
+    : association P
+      (comp (associator X Y Z) (comp (braiding X (Y ⊗ Z)) (associator Y Z X)))
+      (comp (whiskerRight (braiding X Y) Z) (comp (associator Y X Z) (whiskerLeft Y (braiding X Z))))
+  | hoop: association P (comp split join) (identity state')
+  | pure_identity (X): association P (pure (𝟙 X)) (identity _)
+  | pure_composes {X Y Z: C} (f: Value.box X ⟶ Value.box Y) (g: Value.box Y ⟶ Value.box Z)
+    : association P (comp (pure f) (pure g)) (pure (f ≫ g))
+  | pure_left {X Y Z: C} (f: Value.box X ⟶ Value.box Y)
+    : association P (whiskerLeft ⟨Z, 0⟩ (pure f)) (pure (𝒱.whiskerLeft Z f))
+  | pure_right {X Y Z: C} (f: Value.box X ⟶ Value.box Y)
+    : association P (whiskerRight (pure f) ⟨Z, 0⟩) (pure (𝒱.whiskerRight f Z))
+  | pure_associator (X Y Z: C)
+    : association P (@pure C _ _ _ _ _ (𝒱.associator X Y Z).hom) (associator ⟨X, 0⟩ ⟨Y, 0⟩ ⟨Z, 0⟩)
+  | pure_leftUnitor (X: C)
+    : association P (@pure C _ _ _ _ _ (𝒱.leftUnitor X).hom) (leftUnitor ⟨X, 0⟩)
+  | pure_rightUnitor (X: C)
+    : association P (@pure C _ _ _ _ _ (𝒱.rightUnitor X).hom) (rightUnitor ⟨X, 0⟩)
+  | pure_braiding (X Y: C)
+    : association P (@pure C _ _ _ _ _ (𝒮.braiding X Y).hom) (braiding ⟨X, 0⟩ ⟨Y, 0⟩)
+  | effectful_identity (X: C)
+    : association P (effectful (𝟙 X)) (identity ⟨tensorUnit' ⊗ X, 1⟩)
+  --TODO: effectful whiskering?
+  | effectful_inclusion_left {X Y Z: C} (f: X ⟶ Y) (g: Value.box Y ⟶ Value.box Z)
+    : association P 
+      (effectful (f ≫ ℰ.inclusion.map' g))
+      (comp (effectful f) (whiskerLeft state' (pure g)))
+  | effectful_inclusion_right {X Y Z: C} (f: Value.box X ⟶ Value.box Y) (g: Y ⟶ Z)
+    : association P 
+      (effectful (ℰ.inclusion.map' f ≫ g))
+      (comp (whiskerLeft state' (pure f)) (effectful g))
+
+def Diagram.isotopy {C: Type u}
+  [TensorMonoid C]
+  [Category (Value C)]
+  [Category C]
+  [PremonoidalCategory (Value C)]
+  [SymmetricPremonoidalCategory (Value C)]
+  [MonoidalCategory' (Value C)]
+  [PremonoidalCategory C]
+  [EffectfulCategory C]
+  : {X Y: DiagramPort C} -> Diagram X Y -> Diagram X Y -> Prop
+  := Diagram.association (λ_ => True)
 
 inductive Diagram.isotopic {C: Type u}
   [TensorMonoid C]
