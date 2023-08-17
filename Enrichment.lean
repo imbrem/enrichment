cc70d8e2bce7fd3e6f3ad23ba929e28220d89256
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2023-08-17 12:41:26 +0100

    Factored out association, association/congruence weakening

--- Enrichment.lean
+++ Enrichment.lean
@@ -6,3 +6,4 @@ import Enrichment.Premonoidal.Monoidal'
 import Enrichment.Effectful.Category
 import Enrichment.Effectful.Braided
 import Enrichment.Runtime.Diagram
+import Enrichment.Runtime.Association
